1213e71bed4b8dcb2e4fd20769e9a2e6ca354630
Nikolas Santis <nikosantis@gmail.com>
2019-12-05 11:08:43 -0300

    API RESTful and CRUD

---
+++ routes/api/products.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const router = express.Router()
+const productMocks = require('../../utils/mocks/products')
+
+router.get('/', function(req, res) {
+  const { query } = req.query
+
+  res.status(200).json({
+    data: productMocks,
+    message: 'products listed'
+  })
+})
+
+router.get('/:productId', function(req, res) {
+  const { productId } = req.params
+
+  res.status(200).json({
+    data: productMocks[0],
+    message: 'product retrieved'
+  })
+})
+
+router.post('/', function(req, res) {
+
+  res.status(201).json({
+    data: productMocks[0],
+    message: 'product created'
+  })
+})
+
+router.put('/:productId', function(req, res) {
+
+  res.status(200).json({
+    data: productMocks,
+    message: 'product updated'
+  })
+})
+
+router.delete('/', function(req, res) {
+
+  res.status(200).json({
+    data: productMocks[0],
+    message: 'product deleted'
+  })
+})
+
+module.exports = router
