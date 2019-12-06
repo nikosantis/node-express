1e4090ef82802ca4d3314cbe52481c2f84b5f2da
Nikolas Santis <nikosantis@gmail.com>
2019-12-05 22:59:25 -0300

    Connect to mongodb

--- config/index.js
+++ config/index.js
@@ -8,3 +8,5 @@ const config = {
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME
 }
+
+module.exports = { config }
--- services/products.js
+++ services/products.js
@@ -1,12 +1,17 @@
 const productMocks = require('../utils/mocks/products')
+const MongoLib = require('../lib/mongo')
 
 class ProductsService {
   constructor() {
-
+    this.collection = 'products'
+    this.mongoDB = new MongoLib()
   }
 
-  getProducts({ tags }) {
-    return Promise.resolve(productMocks)
+  async getProducts({ tags }) {
+    const query = tags && { tags: { $in: tags } }
+    const products = await this.mongoDB.getAll(this.collection, query)
+
+    return products || []
   }
 
   getProduct({ productId }) {
